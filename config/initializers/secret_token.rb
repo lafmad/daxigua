b98c51438dc7bf2d499d50b34ec38231a374a634
lafmad <inaircastle@gmail.com>
2014-03-17 16:10:15 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Daxigua::Application.config.secret_key_base = 'c3fa46871b7a18cec8356a1443c0ebadb0b883ee84ea3b37037ba843c3937a35f626242abfd96e680bef811c19e1e3559bbecf8ae5a00f4416e271e6180e9270'
